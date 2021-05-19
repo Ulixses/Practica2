aa6929186f35278e278e1ae583a4a910be1f1722
Ulises <alejandro@gonzalez.ws>
2021-05-20 00:27:03 +0200

    Mapa de la tienda y calculo de la distancia

--- app/src/main/java/com/uligra/practica2/DondeActivity.java
+++ app/src/main/java/com/uligra/practica2/DondeActivity.java
@@ -1,13 +1,83 @@
 package com.uligra.practica2;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 
-public class DondeActivity extends BaseActivity {
+public class DondeActivity extends BaseActivity implements OnMapReadyCallback, LocationListener {
 
+    protected LocationManager locationManager;
+    protected LocationListener locationListener;
+    protected Context context;
+    TextView txtLat;
+    String lat;
+    String provider;
+    protected String latitude, longitude;
+    protected boolean gps_enabled, network_enabled;
+    LatLng tienda;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getLayoutInflater().inflate(R.layout.activity_donde, frameLayout);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        txtLat = (TextView) findViewById(R.id.distancia);
+        tienda = new LatLng(41.276897178681885, -3.485096176944683);
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        }
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, this);
     }
 
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+
+
+
+        googleMap.addMarker(new MarkerOptions().position(tienda).title("Nuestra tienda"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(tienda));
+        googleMap.moveCamera(CameraUpdateFactory.zoomTo(15));
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+        float distancia[] = new float[3];
+        Location.distanceBetween(tienda.latitude,tienda.longitude,location.getLatitude(),location.getLongitude(),distancia);
+        txtLat.setText("Distancia a la tienda es de " + distancia[0]/1000 + " kilometros");
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        Log.d("Latitude","disable");
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        Log.d("Latitude","enable");
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        Log.d("Latitude","status");
+    }
 }
