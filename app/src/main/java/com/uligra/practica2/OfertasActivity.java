79d8f67161b9e798d22b5379ed3d157c43c52a64
Graciano <tulaes@hotmail.com>
2021-05-20 00:25:54 +0200

    creadas los spinners para el main sin probar falta meter cosas en la base de datossss MATAMEEEEEEE

--- app/src/main/java/com/uligra/practica2/OfertasActivity.java
+++ app/src/main/java/com/uligra/practica2/OfertasActivity.java
@@ -1,12 +1,15 @@
 package com.uligra.practica2;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
-public class OfertasActivity extends BaseActivity {
+public class
+OfertasActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getLayoutInflater().inflate(R.layout.activity_ofertas, frameLayout);
+        LinearLayout linearLayout = findViewById(R.id.drawerlayout);
     }
 }
