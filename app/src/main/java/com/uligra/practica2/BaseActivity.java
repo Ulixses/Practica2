9788bf6c28d0f5b8ce1410aa608cafc19dde0b40
Ulises <alejandro@gonzalez.ws>
2021-05-19 21:06:09 +0200

    NavigationDrawer funcional

--- app/src/main/java/com/uligra/practica2/BaseActivity.java
+++ app/src/main/java/com/uligra/practica2/BaseActivity.java
@@ -1,14 +1,19 @@
 package com.uligra.practica2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class BaseActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class BaseActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +32,8 @@ public class BaseActivity extends AppCompatActivity {
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
 
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigationdrawer_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
     @Override
@@ -39,5 +46,64 @@ public class BaseActivity extends AppCompatActivity {
     //Buton carro de la compra
     public boolean onOptionsItemSelected (MenuItem item) {
         return utilidades.botonesToolBar(item,this);
+    }
+
+    @Override
+    public void onBackPressed()
+    {
+        DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawerlayout);
+        if (drawerLayout.isDrawerOpen(GravityCompat.START))
+        {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else
+        {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        int id = menuItem.getItemId();
+        Intent intent;
+        switch (id)
+        {
+            case R.id.navigation_home :
+                intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.navigation_ofer :
+                intent = new Intent(this, OfertasActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.navigation_ps4 :
+                intent = new Intent(this, PS4Activity.class);
+                startActivity(intent);
+                break;
+            case R.id.navigation_xbox :
+                intent = new Intent(this, XBoxActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.navigation_new :
+                intent = new Intent(this, NovedadesActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.navigation_shop :
+                intent = new Intent(this, CarroActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.navigation_contact :
+                intent = new Intent(this, ContactoActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.navigation_where :
+                intent = new Intent(this, DondeActivity.class);
+                startActivity(intent);
+                break;
+
+        }
+        DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawerlayout);
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
